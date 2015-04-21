2710e81394558a00cce525a350ef02fdb2d48391
Rui Rei <rui.jorge.rei@googlemail.com>
2015-04-21 13:05:27 +0100

    Fixed bug in PointBehavior.cs

--- Ballz Of Fury/Assets/Scripts/Ballz/PointBehavior.cs
+++ Ballz Of Fury/Assets/Scripts/Ballz/PointBehavior.cs
@@ -4,18 +4,20 @@ using System.Collections;
 
 namespace Ballz {
     public class PointBehavior : MonoBehaviour {
+        public float timeToTarget = 1;
+        private float startTime;
         internal BallInput parent;
 
         void Start() {
             this.transform.localPosition = Vector3.zero;
+            this.startTime = Time.time;
         }
 
         void Update() {
+            var progress = Mathf.Clamp01((Time.time - this.startTime) / this.timeToTarget);
             var distance = this.parent.direction.magnitude;
-            var target = new Vector3(0, 0, distance);
-            this.transform.localPosition += Vector3.forward * distance * Time.deltaTime;
-            if (this.transform.localPosition.z >= distance) {
-                this.transform.localPosition = target;
+            this.transform.localPosition = new Vector3(0, 0, progress * distance);
+            if (progress == 1) {
                 GameObject.Destroy(this.gameObject);
             }
         }
